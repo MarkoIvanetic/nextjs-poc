8043a738d64ff6fbf24bdef801508c738b36586b
MarkoIvanetic <manna.grad@gmail.com>
2021-12-21 15:47:58 +0100

    112 update

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,36 +1,44 @@
-import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { getFeaturedEvents } from "../../utils/data";
-import EventList from "../../components/EventList";
-import EventSearch from "../../components/event-detail/event-search";
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { getEvents } from '../../utils/data'
+import EventList from '../../components/EventList'
+import EventSearch from '../../components/event-detail/event-search'
+import useSWR from 'swr'
 
 const EventsPage = ({ featuredEvents }) => {
-  const router = useRouter();
 
-  function findEventHadler(year, month) {
-    const fullPath = `/events/${year}/${month}`;
-    router.push(fullPath);
+  const { data = featuredEvents, error, isValidating} = useSWR("events", () => getEvents())
+
+	const router = useRouter()
+
+	function findEventHadler(year, month) {
+		const fullPath = `/events/${year}/${month}`
+		router.push(fullPath)
+	}
+
+  if (isValidating) {
+    return <span>Loading...</span>
   }
 
-  return (
-    <>
-      <EventSearch onSearch={findEventHadler} />
-      <EventList events={featuredEvents} />
-    </>
-  );
-};
-
-export async function getStaticProps() {
-  const featuredEvents = getFeaturedEvents();
-
-  console.log(featuredEvents);
-
-  return {
-    props: {
-      featuredEvents
-    }
-  }
+	return (
+		<>
+			<EventSearch onSearch={findEventHadler} />
+			<EventList events={data} />
+		</>
+	)
 }
 
-export default EventsPage;
+export async function getStaticProps() {
+	const featuredEvents = await getEvents()
+
+	console.log(featuredEvents)
+
+	return {
+		props: {
+			featuredEvents,
+		},
+	}
+}
+
+export default EventsPage
